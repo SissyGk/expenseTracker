b97206061ad2ba3480a3813d37dec69279fc98c4
SissyGk <sissygtk@gmail.com>
2025-06-05 02:58:31 +0300

    Initial version

---
+++ main.py
@@ -0,0 +1,109 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.button import Button
+import json
+import os
+
+class trackerLayout(BoxLayout): #inherits from BoxLayout
+    pass
+    #Load previous expenses from json if file exists
+    def load_expenses(self):
+        if os.path.exists("expenses.json"):
+            with open("expenses.json","r")as f:
+                self.expenses=json.load(f)
+        else:
+            self.expenses=[]
+    
+    def add_expense_to_list(self,amount,note):
+        from kivy.clock import Clock
+        label=Label(
+            text=f"{amount:.2f}€ - {note if note else 'No note'}",
+            size_hint_y=None,
+            height=30,
+            halign='left',
+            valign='middle'
+        )
+        label.bind(size=label.setter('text_size')) #Wrap notes
+        self.expense_list_layout.add_widget(label)
+        
+        def scroll_to_bottom(*args):
+            self.scroll_view.scroll_y=0
+        
+        Clock.schedule_once(scroll_to_bottom,0)
+            
+    #initialization        
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.load_expenses() #update expenses list from json file
+        self.orientation='vertical'
+        
+        #Labels
+        self.amount_input = TextInput(hint_text='Enter amount',input_filter='float')
+        self.add_widget(self.amount_input)
+        
+        self.note_input = TextInput(hint_text='Enter note')
+        self.add_widget(self.note_input)
+
+        self.add_button = Button(text='Add Expense')
+        self.add_button.bind(on_press=self.add_expense)
+        self.add_widget(self.add_button)
+
+        self.status = Label(text='No expense added yet')
+        self.add_widget(self.status)        
+        
+        total=sum(exp[0] for exp in self.expenses) #var total with sum of expenses
+        self.sum=Label(text=f"Total Expenses:{total:.2f}€")
+        self.add_widget(self.sum)       
+        self.sum.text=f"Total Expenses:{total:.2f}€"
+        
+        #DISPLAY OF FULL LIST OF EXPENSES
+        self.expense_list_layout = BoxLayout(orientation='vertical',size_hint_y=None)
+        self.expense_list_layout.bind(minimum_height=self.expense_list_layout.setter('height'))
+
+        self.scroll_view=ScrollView(size_hint=(1,1))
+        self.scroll_view.add_widget(self.expense_list_layout)
+        self.add_widget(self.scroll_view)
+        
+        for amount,note in self.expenses:
+            self.add_expense_to_list(amount,note)
+        
+    def save_expenses(self):
+        with open("expenses.json","w") as f:
+            json.dump(self.expenses,f)
+        
+    def add_expense(self,instance):
+        amount_text=self.amount_input.text.strip()
+        note_text=self.note_input.text.strip()
+        
+        if not amount_text:
+            self.status.text="Please enter an amount."
+            return
+        try:
+            amount=float(amount_text)
+            if amount<=0:
+                self.status.text="Amount must be greater than 0."
+                return
+        except ValueError:
+            self.status.text="Invalid amount entered."
+            return
+        
+        self.expenses.append((amount,note_text)) #add expense in list
+        self.add_expense_to_list(amount,note_text)
+        self.status.text=f"Added expense: €{amount:.2f} - {note_text if note_text else 'No note'}"
+        total=sum(exp[0] for exp in self.expenses)
+        self.sum.text=f"Total expenses: {total:.2f}€"
+        self.save_expenses()
+        
+        self.amount_input.text=""
+        self.note_input.text=""
+        
+class trackerApp(App):
+    def build(self):
+        return trackerLayout()
+
+if __name__ == "__main__":
+    trackerApp().run()
+
